c8ced6b5dd78dda5ff94ec7e88825efe95938cbf
rioagungpurnomo <rioagungpurnomo.ak@gmail.com>
2023-04-30 18:07:56 +0700

    Update

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+des = 'PyScraping is a universal web-scraping util for'
+dess = 'Python, built with simplicity in mind.'
+
 setup(
     name='pyscraping',
     version='1.0.0',
-    description='The classic email sending library for Python.',
+    description= des + ' ' + dess,
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Rio Agung Purnomo',
